b3af48a5fe032fcbf0c912002bb186a5f8b49daa
kscheltema <k87scheltema@outlook.com>
2021-07-07 14:49:15 +0200

    link tag prevents auto-reload

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,20 @@
 //our domain.com/news
+import Link from 'next/link';
+import { Fragment } from "react";
 
 function NewsPage () {
-  return <h1>news page</h1>
+  return 
+  <Fragment>
+  <h1>news page</h1>
+  <ul>
+    <li>
+      <Link href='/news/nextjs-is-a-great-framework'>
+      NextJS is a great Framework
+      </Link>
+      </li>
+    <li>Something else</li>
+  </ul>
+  </Fragment>
 };
 
 export default NewsPage;
